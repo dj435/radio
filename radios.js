9490a54bf02affd31a6af445a2fbc0fde4a99916
dj435 <153130191+dj435@users.noreply.github.com>
2025-04-04 03:12:55 -0300

    Add files via upload

---
+++ radios.js
@@ -0,0 +1,10 @@
+        const radios = [
+		
+		    { nombre: "Radio Disney", url: "https://playerservices.streamtheworld.com/api/livestream-redirect/DISNEY_ARG_BA_SC", dial: "94.3" },
+		    { nombre: "FM Terapia", url: "https://streaming2.locucionar.com/proxy/fmterapia987?mp=/stream", dial: "101.5" },
+			{ nombre: "Aspen", url: "https://playerservices.streamtheworld.com/api/livestream-redirect/ASPENAAC_SC", dial: "102.3" },
+			{ nombre: "Los 40", url: "https://frontend.radiohdvivo.com/los40/live", dial: "105.5" },
+            { nombre: "El Destape", url: "https://ipanel.instream.audio/8004/stream", dial: "107.3" }
+            
+            
+        ];
