c64bef59c6dad8226a9504bb78204b9b415028c1
dj435 <153130191+dj435@users.noreply.github.com>
2025-04-07 03:51:34 -0300

    Update radios.js

--- radios.js
+++ radios.js
@@ -1,10 +1,14 @@
         const radios = [
 		
 		    { nombre: "Radio Disney", url: "https://playerservices.streamtheworld.com/api/livestream-redirect/DISNEY_ARG_BA_SC", dial: "94.3" },
+			{ nombre: "Rock&Pop", url: "https://playerservices.streamtheworld.com/api/livestream-redirect/ROCKANDPOPAAC.aac", dial: "95.9" },
 		    { nombre: "FM Terapia", url: "https://streaming2.locucionar.com/proxy/fmterapia987?mp=/stream", dial: "101.5" },
 			{ nombre: "Aspen", url: "https://playerservices.streamtheworld.com/api/livestream-redirect/ASPENAAC_SC", dial: "102.3" },
 			{ nombre: "Los 40", url: "https://frontend.radiohdvivo.com/los40/live", dial: "105.5" },
-            { nombre: "El Destape", url: "https://ipanel.instream.audio/8004/stream", dial: "107.3" }
+			{ nombre: "La Red AM", url: "https://playerservices.streamtheworld.com/api/livestream-redirect/LA_RED_AM910AAC.aac", dial: "910" },
+			{ nombre: "Del Plata", url: "https://streaming01.shockmedia.com.ar:10217/stream", dial: "1030" },
+			{ nombre: "El Mundo AM", url: "https://streaming.escuchanosonline.com:7118//stream?type=http&nocache=1", dial: "1070" },
+            { nombre: "El Destape", url: "https://ipanel.instream.audio/8004/stream", dial: "1070" }
             
             
         ];
